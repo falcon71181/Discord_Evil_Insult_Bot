937d125b822430df8775bc2705b4b2084cb4f2ac
Abhay Thakur <48170773+falcon71181@users.noreply.github.com>
2023-10-02 19:35:07 +0530

    Update run.py

--- run.py
+++ run.py
@@ -1,29 +1,37 @@
 import requests
 import json
-from tkinter import *
+import tkinter as tk
 from PIL import ImageTk, Image
 from datetime import datetime
 
-app = Tk()
+# Create the main application window
+app = tk.Tk()
 app.title("Dev (Parody)")
 app.iconbitmap('icon.ico')
+
+# Initialize row number for grid layout
 row_no = 12
 
+# Load the logo image
 logo_img = ImageTk.PhotoImage(Image.open('gigachad.png'))
 
-def insultfr():
+# Function to fetch a random insult from the API
+def fetch_random_insult():
     url = "https://evilinsult.com/generate_insult.php?lang=en&type=json"
     response = requests.get(url)
     data = response.json()
-    insultapi = data["insult"]
-    return insultapi
+    insult = data["insult"]
+    return insult
 
-def send():
+# Function to send a message using the Discord webhook
+def send_message():
     global row_no
+
     webhook_url = webhook_entry.get()
     user_id = target_entry.get()
     profile_pic_url = profile_pic_entry.get()
-    insult = insultfr()
+    insult = fetch_random_insult()
+
     message = {
         'content': f"<@{user_id}> {insult}",
         'username': botname_entry.get(),
@@ -31,103 +39,110 @@ def send():
     }
 
     payload = json.dumps(message)
-
-    headers = {
-        'Content-Type': 'application/json'
-    }
+    headers = {'Content-Type': 'application/json'}
 
     response = requests.post(webhook_url, data=payload.encode('utf-8'), headers=headers)
 
     if response.status_code == 204:
-        msg = (
-            "[" + datetime.now().strftime('%H:%M:%S') + "] [" + "INFO" + "] " + "Insult sent successfully" + f"\n{insult}")
-        print_label = Label(app, text=msg, fg="green")
+        msg = f"[{datetime.now().strftime('%H:%M:%S')}] [INFO] Insult sent successfully\n{insult}"
+        print_label = tk.Label(app, text=msg, fg="green")
     else:
-        msg = (
-            f"[{datetime.now().strftime('%H:%M:%S')}] [INFO] Failed to send message. Status code: {response.status_code}")
-        print_label = Label(app, text=msg, fg="red")
+        msg = f"[{datetime.now().strftime('%H:%M:%S')}] [INFO] Failed to send message. Status code: {response.status_code}"
+        print_label = tk.Label(app, text=msg, fg="red")
+
     print_label.grid(row=row_no, column=2)
     row_no += 2
 
-def send_custom():
+# Function to send a custom message using the Discord webhook
+def send_custom_message():
     global row_no
+
     webhook_url = webhook_entry.get()
     user_id = target_entry.get()
     profile_pic_url = profile_pic_entry.get()
-    cust_msg = custom_msg_entry.get('1.0', 'end-1c')
+    custom_msg = custom_msg_entry.get('1.0', 'end-1c')
 
     message = {
-        'content': f"<@{user_id}> {cust_msg}",
+        'content': f"<@{user_id}> {custom_msg}",
         'username': botname_entry.get(),
         'avatar_url': profile_pic_url
     }
 
     payload = json.dumps(message)
-
-    headers = {
-        'Content-Type': 'application/json'
-    }
+    headers = {'Content-Type': 'application/json'}
 
     response = requests.post(webhook_url, data=payload.encode('utf-8'), headers=headers)
 
     if response.status_code == 204:
-        print_label = Label(app, text="Message sent successfully", fg="green")
+        print_label = tk.Label(app, text="Message sent successfully", fg="green")
     else:
-        resp_print = (f'Failed to send message. Status code: {response.status_code}')
-        print_label = Label(app, text=resp_print, fg="red")
+        resp_print = f'Failed to send message. Status code: {response.status_code}'
+        print_label = tk.Label(app, text=resp_print, fg="red")
+
     print_label.grid(row=row_no, column=2)
     row_no += 1
 
-webhook_entry = Entry(app, borderwidth=4, bg="#D2CDCC", width=100)
-webhook_entry.insert(0, "dewebhook")
+# Create and configure tkinter widgets
+webhook_entry = tk.Entry(app, borderwidth=4, bg="#D2CDCC", width=100)
+webhook_entry.insert(0, "https://discord.com/api/webhooks/your_webhook_url_here")
 webhook_entry.grid(row=1, column=2)
 
-profile_pic_entry = Entry(app, borderwidth=4, bg="#D2CDCC", width=100)
-profile_pic_entry.insert(0, "pp")
+profile_pic_entry = tk.Entry(app, borderwidth=4, bg="#D2CDCC", width=100)
+profile_pic_entry.insert(0, "https://example.com/your_bot_image.png")
 profile_pic_entry.grid(row=2, column=2)
 
-botname_entry = Entry(app, borderwidth=4, bg="#D2CDCC", width=100)
+botname_entry = tk.Entry(app, borderwidth=4, bg="#D2CDCC", width=100)
 botname_entry.insert(0, "Dev (Parody)")
 botname_entry.grid(row=3, column=2)
 
-target_entry = Entry(app, borderwidth=4, bg="#D2CDCC", width=100)
-target_entry.insert(0, "uid")
+target_entry = tk.Entry(app, borderwidth=4, bg="#D2CDCC", width=100)
+target_entry.insert(0, "715602301632643177")
 target_entry.grid(row=4, column=2)
 
-custom_msg_entry = Text(app, borderwidth=4, bg="#D2CDCC", width=100, height=5)
+custom_msg_entry = tk.Text(app, borderwidth=4, bg="#D2CDCC", width=100, height=5)
 custom_msg_entry.grid(row=5, column=2, rowspan=3)
 
-giga_chad = Label(app, image=logo_img)
+giga_chad = tk.Label(app, image=logo_img)
 giga_chad.grid(row=0, column=0)
 
-welcome = Label(app, text="""
-███████╗░█████╗░██╗░░░░░░█████╗░░█████╗░███╗░░██╗███████╗░░███╗░░░░███╗░░░█████╗░░░███╗░░
-██╔════╝██╔══██╗██║░░░░░██╔══██╗██╔══██╗████╗░██║╚════██║░████║░░░████║░░██╔══██╗░████║░░
-█████╗░░███████║██║░░░░░██║░░╚═╝██║░░██║██╔██╗██║░░░░██╔╝██╔██║░░██╔██║░░╚█████╔╝██╔██║░░
-██╔══╝░░██╔══██║██║░░░░░██║░░██╗██║░░██║██║╚████║░░░██╔╝░╚═╝██║░░╚═╝██║░░██╔══██╗╚═╝██║░░
-██║░░░░░██║░░██║███████╗╚█████╔╝╚█████╔╝██║░╚███║░░██╔╝░░███████╗███████╗╚█████╔╝███████╗
-╚═╝░░░░░╚═╝░░╚═╝╚══════╝░╚════╝░░╚════╝░╚═╝░░╚══╝░░╚═╝░░░╚══════╝╚══════╝░╚════╝░╚══════╝""")
+welcome = tk.Label(app, text="""
+ /¯¯¯¯\/¯¯¯¯¯¯\        |\¯¯¯¯¯\  |\¯¯¯¯¯\'    |\¯¯¯¯¯¯\    /¯¯¯¯¯ /|     °          
+|                      |       |;|         | |;|         |    |;|          |   |          |:'|                
+|         |\ _____ /|'      |/         /  |/        /|'‚   |;|          |   \          \;|                
+'\         \/¯¯¯¯¯¯\'     /          /  /        /;'|'‚   |/          /|‘   \          \|                
+ |        |\______ /|‘   |           \/         /;;:|   /          /;;|'    \          \‘               
+ |        |/¯¯¯¯¯¯¯\‚   |\                   /;;;;;|   |          |;'/°      \          \/¯¯¯¯¯¯\'‚
+ |\____/\ ______/|'   |;;\ ________ /;;;;;/‘   |\_____\/‘         |                      |'‚
+ |;||¯¯¯||'||¯¯¯¯¯|;'|‘   |;;;||¯¯¯¯¯¯¯||;;;;'/      |;||¯¯¯¯||'         /______/\_____/|‘ 
+ |;||     ||'||         |;'|‘    \;;||            ||;;'/        |;||       ||'         ||¯¯¯¯¯||;||¯¯¯¯||;|°
+ '\||___||'||_____|'/‘       \||_______||/    °      \||____||°        ||         ||;||       ||;| 
+    ¯¯     ¯¯¯¯¯°             ¯¯¯¯¯ ‘                ¯¯¯¯°          ||_____||;||____||/  
+               '                                    '                °             ¯¯¯¯      ¯¯¯¯‘   
+      '                               '                     '‚                              ‘               
+                  '        '                                   by '‚falcon71181‘                             
+          '                                     '                     '‚                                   ‘
+""", fg="#FF7801")
 welcome.grid(row=0, column=1, columnspan=8)
 
-ask_webhook = Label(app, text="What's your webhook? ", fg="blue", font=('Times New Roman', 15, 'bold'))
-ask_webhook.grid(row=1, column=0)
+# Labels for input fields
+label_texts = [
+    "What's your webhook? ",
+    "Enter URL for Profile pic for your bot: ",
+    "Enter name for Bot: ",
+    "Enter Target User ID: ",
+    "Enter Custom Message: ",
+]
 
-ask_botprofile_pic = Label(app, text="Enter URL for Profile pic for your bot: ", fg="blue", font=('Times New Roman', 15, 'bold'))
-ask_botprofile_pic.grid(row=2, column=0)
+for i, text in enumerate(label_texts):
+    label = tk.Label(app, text=text, fg="blue", font=('Times New Roman', 15, 'bold'))
+    label.grid(row=i + 1, column=0)
 
-ask_bot_name = Label(app, text="Enter name for Bot: ", fg="blue", font=('Times New Roman', 15, 'bold'))
-ask_bot_name.grid(row=3, column=0)
-
-ask_target = Label(app, text="Enter Target User ID: ", fg="blue", font=('Times New Roman', 15, 'bold'))
-ask_target.grid(row=4, column=0)
-
-ask_custom_msg = Label(app, text="Enter Custom Message: ", fg="blue", font=('Times New Roman', 15, 'bold'))
-ask_custom_msg.grid(row=5, column=0)
-
-send_button = Button(app, text="Send Insult", fg="white", bg="blue", padx=50, pady=5, command=send)
+# Buttons to send insults and custom messages
+send_button = tk.Button(app, text="Send Insult", fg="white", bg="blue", padx=50, pady=5, command=send_message)
 send_button.grid(row=8, column=2, pady=(5, 5))
 
-cus_send_button = Button(app, text="Send Custom", fg="white", bg="blue", padx=50, pady=5, command=send_custom)
+cus_send_button = tk.Button(app, text="Send Custom", fg="white", bg="blue", padx=50, pady=5, command=send_custom_message)
 cus_send_button.grid(row=9, column=2, pady=(5, 0))
 
+# Start the tkinter main loop
 app.mainloop()
